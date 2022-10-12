b7493f5f17ddf60acadd8744ca5caebe58133aec
henry4666 <henrilac22@gmail.com>
2022-10-12 18:50:27 -0300

    call the NFT fuction in run.js

--- scripts/run.js
+++ scripts/run.js
@@ -3,6 +3,12 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
+
+    let txt = await nftContract.makeAnEpicNFT()
+    await txt.wait()
+
+    txt = await nftContract.makeAnEpicNFT()
+    await txt.wait()       
 };
 
 const runMain = async () => {
