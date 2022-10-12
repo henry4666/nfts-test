c6f5c4655211905c0467758dd9440545aa5b1e0c
henry4666 <henrilac22@gmail.com>
2022-10-12 18:34:52 -0300

    add base files made off github

---
+++ contracts/nfts.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+contract nfts is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor () ERC721 ("RandownNFT","RANDONW") {
+        console.log("nice contract");
+    }
+
+    function makeAnEpicNFT () public {
+        unit256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/5F1W");
+
+        _tokenIds.increment();
+    }
+
+}
+
