ce8a2126b70296eae91b3b1dad05a1a0656bd849
henry4666 <henrilac22@gmail.com>
2022-10-12 21:32:46 -0300

    create deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('nfts');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+
+    let txt = await nftContract.makeAnEpicNFT()
+    await txt.wait()
+
+    txt = await nftContract.makeAnEpicNFT()
+    await txt.wait()       
+};
+
+const runMain = async () => {
+    try {
+        await main ();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
