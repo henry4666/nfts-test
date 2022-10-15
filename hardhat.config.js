41928eae2e5f8ce7132929487e396f78c90825b9
henry4666 <henrilac22@gmail.com>
2022-10-14 23:40:27 -0300

    fix hardhat.config method

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 /* @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+module.export = {
   solidity: "0.8.17",
   networks: {
     goerli: {
